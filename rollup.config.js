cae700119d542115a4411277e44b6b79eef33d42
Jeremy Ho <chungying.ho@outland.tw>
2024-11-21 16:43:47 +0800

    feat: setup rollup and bundle

---
+++ rollup.config.js
@@ -0,0 +1,35 @@
+const { terser } = require('rollup-plugin-terser')
+const commonjs = require('@rollup/plugin-commonjs')
+const json = require('@rollup/plugin-json')
+
+module.exports = [
+  {
+    input: 'src/lib/index.js',
+    output: {
+      file: 'dist/index.min.js',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'default'
+    },
+    plugins: [
+      terser(),
+      commonjs()
+    ],
+    external: ['dotenv', 'chalk', 'fs', 'path']
+  },
+  {
+    input: 'src/bin/cli.js',
+    output: {
+      file: 'dist/cli.min.js',
+      format: 'cjs',
+      sourcemap: true,
+      exports: 'default'
+    },
+    plugins: [
+      terser(),
+      commonjs(),
+      json()
+    ],
+    external: ['dotenv', 'chalk', 'fs', 'path', 'commander']
+  }
+]
